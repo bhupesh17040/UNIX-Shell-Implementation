f65864053c8b4c89a0079d45b143cecb67bb7ab2
shashwat <shashwatjain2014@gmail.com>
2019-03-01 14:43:39 +0530

    yo

--- stdin and stdout/stdout.c
+++ stdin and stdout/stdout.c
@@ -1,16 +1,14 @@
-//redirect stdout to file “filename”. If the file does not exist create one,
-//otherwise, overwrite the existing file
+//command < filename
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include<errno.h>
 #include<sys/wait.h>
 #include <unistd.h>
-void redirect( command ,char fn[]){
+void redirect(char command[] , char fn[]){
     // this is written to redir.txt
     freopen(fn,"w",stdout);
-    
-    dup2(fileno(stdin), STDOUT_FILENO);
+    dup2(fileno(stdout), STDOUT_FILENO);
     
     execlp(command,command,NULL);
     fclose(stdout);
@@ -19,6 +17,5 @@ int main(void)
 
 
 {
-    execlp(ls,ls,NULL);
     redirect("ls","redir.txt");
 }
