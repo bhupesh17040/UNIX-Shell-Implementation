f685aa6d1552630a6f7a581c6a8dc635e4e083b7
shashwat <shashwatjain2014@gmail.com>
2019-03-01 14:04:21 +0530

    Update stdin.c

--- stdin and stdout/stdin.c
+++ stdin and stdout/stdin.c
@@ -4,20 +4,19 @@
 #include<errno.h>
 #include<sys/wait.h>
 #include <unistd.h>
+void redirect(char command[] , char fn[]){
+    // this is written to redir.txt
+    freopen(fn,"r",stdin);
+    dup2(fileno(stdin), STDIN_FILENO);
+    
+    execlp(command,command,NULL);
+    fclose(stdin);
+}
 int main(void)
 
+
 {
-        if (freopen("redir.txt", "r", stdin) == NULL)
-        {
-            perror("freopen() failed");
-            return EXIT_FAILURE;
-        }
-        // this is written to redir.txt
-       
-        dup2(fileno(stdin), STDIN_FILENO);
-        
-        execlp("wc","wc",NULL);
-        fclose(stdin);
+    redirect("wc","redir.txt");
     }
 
 
